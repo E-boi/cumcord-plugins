2d6d8e3fb417c05415dff5bd4895bd41b0f143e1
e-boi <hiboysismyoufake6789@gmail.com>
2021-09-29 17:48:36 -0400

    update sussy host

--- amongus/index.js
+++ amongus/index.js
@@ -1,8 +1,10 @@
-// import { webpack } from '@cumcord/modules';
 import { React } from '@cumcord/modules/common';
+import css from './style.css';
 import Settings from './Settings';
-const sussy = new Audio('https://raw.githubusercontent.com/E-boi/among-us/main/sussy.mp3');
+const sussy = new Audio('https://cdn.discordapp.com/attachments/770304534203334678/892889666674634822/sussy.mp3');
 sussy.loop = true;
+
+let cssInject;
 
 function updateVolume(volume, baka) {
 	baka.volume = volume;
@@ -11,10 +13,14 @@ function updateVolume(volume, baka) {
 export default ({ persist }) => {
 	return {
 		onLoad() {
+			cssInject = css();
 			sussy.volume = persist.ghost.volume ?? 1;
 			sussy.play();
 		},
-		onUnload: () => sussy.pause(),
+		onUnload: () => {
+			cssInject?.();
+			sussy.pause();
+		},
 		settings: React.createElement(Settings, { updateVolume, persist, sussy }),
 	};
 };
