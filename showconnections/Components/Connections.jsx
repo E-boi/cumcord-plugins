7df99a9bb16fcdf72b4545d0ff2a81c741c7db18
e-boi <hiboysismyoufake6789@gmail.com>
2021-09-30 21:20:35 -0400

    damn console log

--- showconnections/Components/Connections.jsx
+++ showconnections/Components/Connections.jsx
@@ -15,7 +15,6 @@ export default ({ user }) => {
 	if (!connections) fetchProfile(user, 'cumcord', user => setConnections(user.connected_accounts));
 
 	if (!connections?.length > 0) return null;
-	console.log(connections);
 	return [
 		<h3 className={[classes.base, classes.muted, classes.uppercase, classes.size12, classes.bodyTitle].join(' ')}>Connections</h3>,
 		<div className={`sc-connectAccounts ${classes.scrollbarGhostHairline}`}>
