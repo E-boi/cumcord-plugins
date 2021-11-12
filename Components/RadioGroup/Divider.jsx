ce7d2c436666a459bd5f82b2497b645fd97280af
e-boi <hiboysismyoufake6789@gmail.com>
2021-11-11 20:29:56 -0500

    update divider class

--- Components/RadioGroup/Divider.jsx
+++ Components/RadioGroup/Divider.jsx
@@ -1,8 +1,7 @@
 import { webpack } from '@cumcord/modules';
 
 const classes = {
-	divider: webpack.findByPropsAll('divider')[1].divider,
 	dividerDefault: webpack.findByProps('dividerDefault').dividerDefault,
 };
 
-export default ({ className }) => <div className={`${classes.divider} ${classes.dividerDefault} ${className}`} />;
+export default ({ className }) => <div className={`divider-3573oO ${classes.dividerDefault} ${className}`} />;
