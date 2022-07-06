7eddbd0e2893dd4f4a9d0db7b7fa2e40aee0bf22
E-boi <eboi.email.official@gmail.com>
2022-07-06 16:07:41 -0400

    [Show Connections] do something about unknown platform

--- showconnections/Components/Connection.jsx
+++ showconnections/Components/Connection.jsx
@@ -2,21 +2,21 @@ import { webpack } from '@cumcord/modules';
 import { i18n } from '@cumcord/modules/common';
 
 const { TooltipContainer } = webpack.findByProps('TooltipContainer');
-
-const { get } = webpack.findByProps('get', 'isSupported');
+const { get, isSupported } = webpack.findByProps('get', 'isSupported');
 
 export default ({ account }) => {
-	const classes = webpack.findByProps('connectedAccount');
-	const connection = get(account.type);
-	return (
-		<TooltipContainer className='sc-connection scrollbarGhostHairline-1mSOM1' text={account.name}>
-			<a target='_blank' href={connection.getPlatformUserUrl?.(account)}>
-				<img
-					alt={i18n.Messages.IMG_ALT_LOGO.format({ name: connection?.name })}
-					className={classes.connectedAccountIcon}
-					src={connection?.icon?.darkPNG}
-				/>
-			</a>
-		</TooltipContainer>
-	);
+  if (!isSupported(account.type)) return null;
+  const classes = webpack.findByProps('connectedAccount');
+  const connection = get(account.type);
+  return (
+    <TooltipContainer className='sc-connection scrollbarGhostHairline-1mSOM1' text={account.name}>
+      <a target='_blank' href={connection.getPlatformUserUrl?.(account)}>
+        <img
+          alt={i18n.Messages.IMG_ALT_LOGO.format({ name: connection?.name })}
+          className={classes.connectedAccountIcon}
+          src={connection?.icon?.darkSVG}
+        />
+      </a>
+    </TooltipContainer>
+  );
 };
