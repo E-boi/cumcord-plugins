dc620ec0a069a6bd7790e9a45c1ccf8d88499c3a
E-boi <eboi.email.official@gmail.com>
2022-07-11 13:23:20 -0400

    remove comments

--- showconnections/index.js
+++ showconnections/index.js
@@ -20,11 +20,6 @@ export default () => {
         res.props.children.splice(2, 0, React.createElement(Connections, { user: user.id }));
         return res;
       });
-      // console.log(Popout);
-      // injection = before('default', Popout, args => {
-      //   console.log(args);
-      //   return args;
-      // });
     },
     onUnload() {
       injection?.();
