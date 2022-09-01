34986a48d363ffff409d6f5eca48cec1deb218d7
e-boi <eboi.email.official@gmail.com>
2022-09-01 15:28:23 -0400

    [Show connections] use different function to get connections

--- showconnections/Components/Connections.jsx
+++ showconnections/Components/Connections.jsx
@@ -6,20 +6,17 @@ const classes = {
   ...webpack.findByProps('scrollbarGhostHairline'),
   ...webpack.findByProps('title', 'body'),
 };
-const { fetchProfile } = webpack.findByProps('fetchProfile');
+const { getUserProfile } = webpack.findByProps('getUserProfile');
 const { Heading } = webpack.findByProps('Heading');
 
 export default ({ user, skin }) => {
-  const [connections, setConnections] = React.useState();
-
-  if (!connections) fetchProfile(user, 'cumcord', user => setConnections(user.connected_accounts));
-
+  const connections = getUserProfile(user).connectedAccounts;
   if (!connections?.length > 0) return null;
+
   return [
     <Heading className={skin ? classes.title : classes.bodyTitle} color={!skin && 'header-secondary'} level={3} variant='eyebrow'>
       Connections
     </Heading>,
-    // <h3 className={[classes.base, classes.muted, classes.uppercase, classes.size12, classes.bodyTitle].join(' ')}>Connections</h3>,
     <div className={`sc-connectAccounts ${classes.scrollbarGhostHairline}`}>
       {connections.map(account => (
         <Connection account={account} />
