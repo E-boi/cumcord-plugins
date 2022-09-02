03e3bcf94394648555b91f0bbe691dca1878711c
e-boi <eboi.email.official@gmail.com>
2022-09-02 10:13:51 -0400

    [Channel Menu] module lazy loads

--- channel-menu/index.jsx
+++ channel-menu/index.jsx
@@ -1,5 +1,5 @@
 import { webpack } from '@cumcord/modules';
-import { after } from '@cumcord/patcher';
+import { after, findAndPatch } from '@cumcord/patcher';
 import { Routes } from '@cumcord/modules/common/constants';
 
 const patches = [];
@@ -10,32 +10,62 @@ const { transitionTo } = webpack.findByProps('transitionTo');
 export default {
   onLoad() {
     patches.push(
-      after('default', webpack.findByDisplayName('GuildContextMenuWrapper', false), (_, res) => {
-        after(
-          'type',
-          res.props.children,
-          ([{ guild }], res) => {
-            const channels = getChannels(guild.id).SELECTABLE;
-            res.props.children.splice(
-              3,
-              0,
-              <MenuItem id='guild-channels' label='Channels'>
-                {channels.map(({ comparator, channel }) => (
-                  <MenuItem
-                    id={`guild-channel-${comparator}`}
-                    label={channel.name}
-                    action={() => transitionTo(Routes.CHANNEL(guild.id, channel.id))}
-                  />
-                ))}
-              </MenuItem>
+      findAndPatch(
+        () => webpack.findByDisplayName('GuildContextMenuWrapper', false),
+        m =>
+          after('default', m, (_, res) => {
+            after(
+              'type',
+              res.props.children,
+              ([{ guild }], res) => {
+                const channels = getChannels(guild.id).SELECTABLE;
+                res.props.children.splice(
+                  3,
+                  0,
+                  <MenuItem id='guild-channels' label='Channels'>
+                    {channels.map(({ comparator, channel }) => (
+                      <MenuItem
+                        id={`guild-channel-${comparator}`}
+                        label={channel.name}
+                        action={() => transitionTo(Routes.CHANNEL(guild.id, channel.id))}
+                      />
+                    ))}
+                  </MenuItem>
+                );
+                return res;
+              },
+              true
             );
             return res;
-          },
-          true
-        );
-        return res;
-      })
+          })
+      )
     );
+    //   after('default', webpack.findByDisplayName('GuildContextMenuWrapper', false), (_, res) => {
+    //     after(
+    //       'type',
+    //       res.props.children,
+    //       ([{ guild }], res) => {
+    //         const channels = getChannels(guild.id).SELECTABLE;
+    //         res.props.children.splice(
+    //           3,
+    //           0,
+    //           <MenuItem id='guild-channels' label='Channels'>
+    //             {channels.map(({ comparator, channel }) => (
+    //               <MenuItem
+    //                 id={`guild-channel-${comparator}`}
+    //                 label={channel.name}
+    //                 action={() => transitionTo(Routes.CHANNEL(guild.id, channel.id))}
+    //               />
+    //             ))}
+    //           </MenuItem>
+    //         );
+    //         return res;
+    //       },
+    //       true
+    //     );
+    //     return res;
+    //   })
+    // );
   },
   onUnload() {
     console.log(patches);
