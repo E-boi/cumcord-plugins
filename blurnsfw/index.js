204afd6385f01f1726faf74008be305b1d239e8b
e-boi <eboi.email.official@gmail.com>
2022-09-09 17:49:08 -0400

    [BlurNSFW] update

--- blurnsfw/index.js
+++ blurnsfw/index.js
@@ -2,7 +2,8 @@ import { after } from '@cumcord/patcher';
 import { webpack } from '@cumcord/modules';
 import { React, FluxDispatcher } from '@cumcord/modules/common';
 import css from './style.css';
-import settings from './settings';
+import Settings from './settings';
+import { persist } from '@cumcord/pluginData';
 
 const injections = [];
 let cssInject;
@@ -11,47 +12,43 @@ const { getChannel } = webpack.findByProps('getChannel', 'getDMFromUserId');
 
 function handleChannel({ channelId }) {
   const channel = getChannel(channelId);
-  if (this.ghost.blocked?.includes(channelId) || this.ghost.blocked?.includes(channel.recipients[0])) return;
+  if (!channel || persist.ghost.blocked?.includes(channelId) || persist.ghost.blocked?.includes(channel.recipients[0])) return;
   else if (
     channel.nsfw ||
-    (channel.type === 1 && this.ghost.dm) ||
-    (channel.type === 3 && this.ghost.gc) ||
-    this.ghost.blurChannels?.includes(channel.id) ||
-    this.ghost.blurChannels?.includes(channel.recipients[0])
+    (channel.type === 1 && persist.ghost.dm) ||
+    (channel.type === 3 && persist.ghost.gc) ||
+    persist.ghost.blurChannels?.includes(channel.id) ||
+    persist.ghost.blurChannels?.includes(channel.recipients[0])
   )
     blurChannel();
   else document.body.classList.remove('blur');
 }
 
-export default ({ persist }) => {
-  handleChannel = handleChannel.bind(persist);
-  blurChannel = blurChannel.bind(persist);
-  return {
-    onLoad() {
-      cssInject = css();
-      injections.push(
-        after('default', ChannelItem, ([{ channel }], res) => {
-          if (!channel.nsfw || persist.ghost.notags) return;
-          res.props.children.props.children[1].props.children[1].props.children.push(
-            React.createElement('div', { className: 'nsfw-badge' }, React.createElement('div', { className: 'nsfw-text' }, 'NSFW'))
-          );
-        })
-      );
+export default {
+  onLoad() {
+    cssInject = css();
+    injections.push(
+      after('default', ChannelItem, ([{ channel }], res) => {
+        if (!channel.nsfw || persist.ghost.notags) return;
+        res.props.children.props.children[1].props.children[1].props.children.push(
+          React.createElement('div', { className: 'nsfw-badge' }, React.createElement('div', { className: 'nsfw-text' }, 'NSFW'))
+        );
+      })
+    );
 
-      FluxDispatcher.subscribe('CHANNEL_SELECT', handleChannel);
-    },
-    onUnload() {
-      FluxDispatcher.unsubscribe('CHANNEL_SELECT', handleChannel);
-      cssInject?.();
-      injections.forEach(i => i());
-    },
-    settings: React.createElement(settings, { persist }),
-  };
+    FluxDispatcher.subscribe('CHANNEL_SELECT', handleChannel);
+  },
+  onUnload() {
+    FluxDispatcher.unsubscribe('CHANNEL_SELECT', handleChannel);
+    cssInject?.();
+    injections.forEach(i => i());
+  },
+  settings: Settings,
 };
 
 function blurChannel() {
-  const blur = this.ghost.blur || 10;
-  const timing = this.ghost.timing || 1;
+  const blur = persist.ghost.blur || 10;
+  const timing = persist.ghost.timing || 1;
   const element = document.body;
   element.style.setProperty('--blur-effect', `blur(${blur}px)`);
   element.style.setProperty('--blur-timing', `${timing}s`);
